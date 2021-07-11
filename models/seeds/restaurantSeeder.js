a1a1aebbfd9d3d59a674f561580d8f19fb181521
steven4program <stevenchang421@gmail.com>
2021-07-11 22:44:20 +0800

    feat: create restaurants database

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Restaurant = require('../restaurant');
+const rawInfo = require('./restaurant.json');
+
+mongoose.connect('mongodb://localhost/restaurant-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('mongodb error!');
+});
+
+db.once('open', () => {
+  console.log('mongodb connected!');
+  rawInfo.results.forEach((restaurant) => {
+    Restaurant.create({
+      id: restaurant.id,
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description,
+    });
+  });
+  console.log('done!');
+});
