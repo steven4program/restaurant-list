c65acfdc90c1e917e4ec4ed19425e668c0c02847
steven4program <stevenchang421@gmail.com>
2021-08-10 22:23:01 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
-// 將網址結構符合 / 字串的 request 導向 home module
+// 將網址結構符合特定字串導向對應的module
 router.use('/', home)
-// 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants module
 router.use('/restaurants', restaurants)
+router.use('/users', users)
+
 // 匯出 route module
 module.exports = router
