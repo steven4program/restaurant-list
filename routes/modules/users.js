73082b9e22ceaf2a30701463238d76fc8433e519
steven4program <stevenchang421@gmail.com>
2021-08-10 22:25:35 +0800

    feat: add login and register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,5 +4,10 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+router.post('/login', (req, res) => {})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 module.exports = router
